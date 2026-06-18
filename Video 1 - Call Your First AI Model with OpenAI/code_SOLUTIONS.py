28c5336a0d254be7e370653918b911e858bd805f
Dylan Song <dylanhgs@gmail.com>
2026-06-18 12:50:40 -0700

    Created the basic call to the AI model

--- Video 1 - Call Your First AI Model with OpenAI/code_SOLUTIONS.py
+++ Video 1 - Call Your First AI Model with OpenAI/code_SOLUTIONS.py
@@ -3,3 +3,13 @@ from openai import OpenAI
 
 load_dotenv()
 openai = OpenAI()
+
+system_prompt = 'You are an expert Python developer that is providing assistant to beginners learning programming for the first time.'
+user_prompt = 'Explain what a Python variable is in one sentence.'
+
+response = openai.chat.completions.create(
+    model='gpt-4.1-mini',
+    messages=[{'role': 'system', 'content': system_prompt},{'role': 'user', 'content': user_prompt}]
+)
+
+print(response.choices[0].message.content)
